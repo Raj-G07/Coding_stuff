c34b65898d9243ffdf5b15ed5c6377a4e0c1c50e
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-10-21 18:45:15 +0530

    added header files

---
+++ include/algorithms/graph/dfs.h
@@ -0,0 +1,5 @@
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {}
---
+++ include/algorithms/graph/dijkstra's.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+void dijkstra(int src, int n, vector<vector<pair<int, int>>> &adj){}
