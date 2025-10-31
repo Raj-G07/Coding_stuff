ebc368a06e5f7b6de35fee50f375a71ff21afb22
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-10-31 17:50:57 +0530

    Height of binary tree

---
+++ src/data_structures/trees/binary_tree/height.cpp
@@ -0,0 +1,11 @@
+#include "./structure.cpp"
+
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return max(leftHeight, rightHeight) + 1;
+}
+
